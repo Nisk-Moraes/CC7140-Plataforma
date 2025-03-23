245759960cf977a5497a8848541a69acb9eeeef6
joca2511 <simoesruy@gmail.com>
2025-03-23 15:10:05 -0300

    Adicionado plataforma

--- platformer/Assets/Plataforma.cs
+++ platformer/Assets/Plataforma.cs
@@ -6,17 +6,27 @@ public class Plataforma : MonoBehaviour
 {
     // Start is called before the first frame update
     public float tempomax = 1.0f;
-    public float time = 0.0f;
+    public float velocidade = 1.0f;
+    private float time = 0.0f;
     public Rigidbody2D rb2d;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        var vel = rb2d.velocity;
+        vel.x = velocidade;
+        rb2d.velocity = vel;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        var vel = rb2d.velocity;
+        time += Time.deltaTime;
+        if (time >= tempomax ){
+            time = 0;
+            vel.x *= -1;
+            rb2d.velocity = vel;
+        }
     }
 }
