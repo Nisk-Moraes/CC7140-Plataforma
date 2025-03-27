7f4f2fea70d622d9f222845c35e132cf2fce24c7
joca2511 <simoesruy@gmail.com>
2025-03-27 12:40:15 -0300

    Pulo, Corrida e inimigo (beta)

--- platformer/Assets/GameManager.cs
+++ platformer/Assets/GameManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static int Pontuacao = 0;
+    
+    public GUISkin layout;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +17,10 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+    void OnGUI()
+    {
+        GUI.skin = layout;
+        GUI.Label(new Rect(Screen.width / 2 - 200 - 12, 40, 100, 100), "Score: " + Pontuacao);
     }
 }
--- platformer/Assets/movJog.cs
+++ platformer/Assets/movJog.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
 
 public class movJog : MonoBehaviour
@@ -7,15 +8,19 @@ public class movJog : MonoBehaviour
     public KeyCode dir = KeyCode.D;
     public KeyCode esq = KeyCode.A;
     public KeyCode pulo = KeyCode.Space;
+    public KeyCode correr = KeyCode.LeftShift;
     public Rigidbody2D rb2d;
     public float tampul = 10f;
-    private bool japulou = false;
+    private int japulou = 0;
+    public int maxpulos = 1;
     public float velocidade = 2.0f;
+    public float velcorrer = 4.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
@@ -25,20 +30,31 @@ public class movJog : MonoBehaviour
         print((int)vel.y);
 
        
-        if (Input.GetKeyDown(pulo) && !japulou){
+        if (Input.GetKeyDown(pulo) && japulou < maxpulos){
             vel.y = tampul;
-            japulou = true;
+            japulou++;
         }
         if (Input.GetKey(dir)){
-            vel.x = velocidade;
+            if (Input.GetKey(correr)){
+                vel.x = velcorrer;
+            }
+            else{
+                vel.x = velocidade;
+            }
+            
         }
         if (Input.GetKey(esq)){
-            vel.x = -velocidade;
+            if (Input.GetKey(correr)){
+                vel.x = -velcorrer;
+            }
+            else{
+                vel.x = -velocidade;
+            }
         }
         rb2d.velocity = vel;
     }
     void OnCollisionEnter2D(Collision2D other) {
-        japulou = false;
+        japulou = 0;
         
     }
 }
